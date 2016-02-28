d7cc3ebf7e0c103c290758adc98e8194c5f74b68
Samuel Pastva <daemontus@gmail.com>
2016-02-28 17:11:20 +0100

    Better integration tests

--- src/test/kotlin/cz/muni/fi/ode/generator/IntegrationTest.kt
+++ src/test/kotlin/cz/muni/fi/ode/generator/IntegrationTest.kt
@@ -1,5 +1,5 @@
 package cz.muni.fi.ode.generator
-/*
+
 import cz.muni.fi.checker.IDNode
 import cz.muni.fi.checker.UniformPartitionFunction
 import cz.muni.fi.ode.model.Parser
@@ -7,7 +7,7 @@ import cz.muni.fi.ode.model.computeApproximation
 import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
-*/
+
 /**
  * This test takes previously computed results for known models and compares them to values obtained from
  * the new generator.
@@ -18,28 +18,13 @@ import kotlin.test.assertEquals
  * Note: don't use these tests as benchmarks - the JIT compiler can brutally optimize whole process
  * but one test is not enough to see the results...
  *
- * If you want to create/regenerate integration tests, use this code snippet to reproduce the format:
+ * If you want to create/regenerate integration tests, use attached main function.
  *
- * val name = "model_31_reduced"
- * val writer = File("models/$name.transitions.txt").outputStream().bufferedWriter()
- * val model = Parser().parse(File("models/$name.bio")).computeApproximation()
- * val fragment = OdeFragment(model, UniformPartitionFunction<IDNode>())
- * for (node in fragment.allNodes().entries) {
- *    writer.write("Successors for ${node.key}:\n")
- *    for (successor in fragment.successors.invoke(node.key).entries) {
- *       writer.write("\t${successor.key} - ${successor.value}\n")
- *    }
- *    writer.write("Predecessors for ${node.key}:\n")
- *    for (predecessor in fragment.predecessors.invoke(node.key).entries) {
- *       writer.write("\t${predecessor.key} - ${predecessor.value}\n")
- *    }
- * }
- * writer.close()
- *
- * Tests are disabled by default, because they are not platform independent (ordering of nodes can change).
+ * Note that ordering of nodes is preserved and each color set should have unique string representation
+ * since it has only one rectangle.
  */
 
-/*
+
 class IntegrationTest {
 
     @Test fun TCBBTest() = integrationTest(File("models/tcbb.bio"), File("models/tcbb.transitions.txt"))
@@ -53,13 +38,13 @@ class IntegrationTest {
 
         val fragment = OdeFragment(model, UniformPartitionFunction<IDNode>())
 
-        for (node in fragment.allNodes().entries) {
+        for (node in fragment.allNodes().entries.toList().sortedBy { it.key.id }) {
             assertEquals(results.readLine(), "Successors for ${node.key}:")
-            for (successor in fragment.successors.invoke(node.key).entries) {
+            for (successor in fragment.successors.invoke(node.key).entries.toList().sortedBy { it.key.id }) {
                 assertEquals(results.readLine(), "\t${successor.key} - ${successor.value}")
             }
             assertEquals(results.readLine(), "Predecessors for ${node.key}:")
-            for (predecessor in fragment.predecessors.invoke(node.key).entries) {
+            for (predecessor in fragment.predecessors.invoke(node.key).entries.toList().sortedBy { it.key.id }) {
                 assertEquals(results.readLine(), "\t${predecessor.key} - ${predecessor.value}")
             }
         }
@@ -67,4 +52,3 @@ class IntegrationTest {
     }
 
 }
-*/
