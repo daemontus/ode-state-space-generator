40ee931acc08f364f713f6285f6d21585f7df786
Samuel Pastva <daemontus@gmail.com>
2016-11-14 22:10:19 +0100

    A serializer into json config format

--- src/main/java/com/github/sybila/ode/model/Writer.kt
+++ src/main/java/com/github/sybila/ode/model/Writer.kt
@@ -3,23 +3,24 @@ package com.github.sybila.ode.model
 fun Model.toBio(): String {
     return (if (this.parameters.isNotEmpty()) {
                 "PARAMS: ${this.parameters.map {
-                    "${it.name}, ${it.range.first}, ${it.range.second}"
+                    "${it.name}, ${it.range.first.safeString()}, ${it.range.second.safeString()}"
                 }.joinToString(separator = ";")}"
             } else "") + "\n" +
             ("VARS: " + this.variables.map { it.name }.joinToString()) + "\n" +
             (this.variables.map {
-                "THRES: ${it.name}: ${it.thresholds.joinToString()}"
+                "THRES: ${it.name}: ${it.thresholds.map(Double::safeString).joinToString()}"
             }.joinToString(separator = "\n")) + "\n" +
             (this.variables.map {
                 "EQ: ${it.name} = ${it.equation.map {
-                    "${it.constant}" +
+                    it.constant.safeString() +
                     (if (it.hasParam()) { " * " + this.parameters[it.paramIndex].name } else "") +
                     it.variableIndices.map { " * " + this.variables[it].name }.joinToString(separator = "") +
                     it.evaluable.map {
                         if (it is RampApproximation) {
                             " * Approx(${this.variables[it.varIndex].name})(${
-                                it.thresholds.zip(it.values).map { "[${it.first}, ${it.second}]" }
-                                .joinToString()
+                                it.thresholds.zip(it.values).map {
+                                    "[${it.first.safeString()}, ${it.second.safeString()}]"
+                                }.joinToString()
                             })"
                         } else {
                             throw IllegalArgumentException("Cannot serialize models with non-ramp functions.")
@@ -27,4 +28,8 @@ fun Model.toBio(): String {
                     }.joinToString(separator = "")
                 }.joinToString(separator = " + ")}"
             }.joinToString(separator = "\n"))
+}
+
+fun Double.safeString(): String {
+    return String.format("%f", this)
 }
