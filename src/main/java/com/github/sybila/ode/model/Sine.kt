30c076ee43084a68cac79b4e4f9ecefb469f94d8
martindemko <325073@mail.muni.cz>
2017-05-17 17:07:23 +0200

    removed_redundant_constructor

--- src/main/java/com/github/sybila/ode/model/Sine.kt
+++ src/main/java/com/github/sybila/ode/model/Sine.kt
@@ -8,8 +8,6 @@ data class Sine private constructor(
         override val varIndex: Int
 ) : Evaluable {
 
-    constructor(varIndex: Int): this(varIndex)
-
     override fun eval(value: Double): Double {
         return Math.sin(value)
     }
