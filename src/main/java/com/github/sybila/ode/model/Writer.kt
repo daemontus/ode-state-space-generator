94a3da10b8e5be5c6e1ffdd62d1c9abd1f159483
Samuel Pastva <daemontus@gmail.com>
2018-04-25 16:43:45 +0200

    add serialisation of step function

--- src/main/java/com/github/sybila/ode/model/Writer.kt
+++ src/main/java/com/github/sybila/ode/model/Writer.kt
@@ -4,30 +4,34 @@ import com.github.sybila.ode.safeString
 
 fun OdeModel.toBio(): String {
     return (if (this.parameters.isNotEmpty()) {
-                "PARAMS: ${this.parameters.map {
+                "PARAMS: ${this.parameters.joinToString(separator = ";") {
                     "${it.name}, ${it.range.first.safeString()}, ${it.range.second.safeString()}"
-                }.joinToString(separator = ";")}"
+                }}"
             } else "") + "\n" +
-            ("VARS: " + this.variables.map { it.name }.joinToString()) + "\n" +
-            (this.variables.map {
-                "THRES: ${it.name}: ${it.thresholds.map(Double::safeString).joinToString()}"
-            }.joinToString(separator = "\n")) + "\n" +
-            (this.variables.map {
-                "EQ: ${it.name} = ${it.equation.map {
+            ("VARS: " + this.variables.joinToString { it.name }) + "\n" +
+            (this.variables.joinToString(separator = "\n") {
+                "THRES: ${it.name}: ${it.thresholds.joinToString(transform = Double::safeString)}"
+            }) + "\n" +
+            (this.variables.joinToString(separator = "\n") {
+                "EQ: ${it.name} = ${it.equation.joinToString(separator = " + ") {
                     it.constant.safeString() +
-                    (if (it.hasParam()) { " * " + this.parameters[it.paramIndex].name } else "") +
-                    it.variableIndices.map { " * " + this.variables[it].name }.joinToString(separator = "") +
-                    it.evaluable.map {
-                        if (it is RampApproximation) {
-                            " * Approx(${this.variables[it.varIndex].name})(${
-                                it.thresholds.zip(it.values).map {
-                                    "[${it.first.safeString()}, ${it.second.safeString()}]"
-                                }.joinToString()
-                            })"
-                        } else {
-                            throw IllegalArgumentException("Cannot serialize models with non-ramp functions.")
-                        }
-                    }.joinToString(separator = "")
-                }.joinToString(separator = " + ")}"
-            }.joinToString(separator = "\n"))
+                            (if (it.hasParam()) { " * " + this.parameters[it.paramIndex].name } else "") +
+                            it.variableIndices.joinToString(separator = "") { " * " + this.variables[it].name } +
+                            it.evaluable.joinToString(separator = "") {
+                                when (it) {
+                                    is RampApproximation -> {
+                                        " * Approx(${this.variables[it.varIndex].name})(${
+                                        it.thresholds.zip(it.values).joinToString {
+                                            "[${it.first.safeString()}, ${it.second.safeString()}]"
+                                        }
+                                        })"
+                                    }
+                                    is Step -> {
+                                        " * H${if (it.a < it.b) "p" else "m"}(${it.theta}, ${it.a}, ${it.b})"
+                                    }
+                                    else -> throw IllegalArgumentException("Cannot serialize models with non-ramp functions.")
+                                }
+                            }
+                }}"
+            })
 }
