e10ef89651cdd1b1197df5e414c83686fc317491
xracek6 <jakub.racek98@gmail.com>
2019-03-14 13:31:11 +0100

    new test cases

--- src/test/kotlin/com/github/sybila/ode/generator/v2/SimpleOdeTransitionSystemTwoDimNoParamTest.kt
+++ src/test/kotlin/com/github/sybila/ode/generator/v2/SimpleOdeTransitionSystemTwoDimNoParamTest.kt
@@ -237,34 +237,53 @@ class SimpleOdeTransitionSystemTwoDimNoParamTest {
         }
     }
 
-    /*
-        ->
-       ^  |
-       |  v
-        <-
-    */
     @Test
     fun case7() {
-        createFragment(listOf(-1.0, -1.0, 1.0, 1.0), listOf(1.0, 1.0, -1.0, -1.0)).run {
-            checkSuccessors(0, setOf(3))
-            checkSuccessors(1, setOf(0, 4))
-            checkSuccessors(2, setOf(1))
-            checkSuccessors(3, setOf(4, 6))
+        createFragment(listOf(1.0, -1.0, 1.0, -1.0), listOf(1.0, -1.0, 1.0, -1.0)).run {
+            checkSuccessors(0, setOf(0, 1, 3))
+            checkSuccessors(1, setOf(0, 1, 2, 4))
+            checkSuccessors(2, setOf(1, 5, 2))
+            checkSuccessors(3, setOf(0, 3, 4, 6))
             checkSuccessors(4, setOf(1, 3, 4, 5, 7))
-            checkSuccessors(5, setOf(4, 2))
-            checkSuccessors(6, setOf(7))
-            checkSuccessors(7, setOf(4, 8))
-            checkSuccessors(8, setOf(5))
+            checkSuccessors(5, setOf(2, 4, 5, 8))
+            checkSuccessors(6, setOf(3, 6, 7))
+            checkSuccessors(7, setOf(4, 6, 7, 8))
+            checkSuccessors(8, setOf(5, 7, 8))
 
-            checkPredecessors(0, setOf(1))
-            checkPredecessors(1, setOf(2, 4))
-            checkPredecessors(2, setOf(5))
-            checkPredecessors(3, setOf(0, 4))
+            checkPredecessors(0, setOf(0, 1, 3))
+            checkPredecessors(1, setOf(0, 1, 2, 4))
+            checkPredecessors(2, setOf(1, 2, 5))
+            checkPredecessors(3, setOf(0, 3, 4, 6))
             checkPredecessors(4, setOf(1, 3, 4, 5, 7))
-            checkPredecessors(5, setOf(4, 8))
-            checkPredecessors(6, setOf(3))
-            checkPredecessors(7, setOf(4, 6))
-            checkPredecessors(8, setOf(7))
+            checkPredecessors(5, setOf(2, 4, 5, 8))
+            checkPredecessors(6, setOf(3, 6, 7))
+            checkPredecessors(7, setOf(4, 6, 7, 8))
+            checkPredecessors(8, setOf(5, 7, 8))
+        }
+    }
+
+    @Test
+    fun case8() {
+        createFragment(listOf(-1.0, 1.0, -1.0, 1.0), listOf(-1.0, 1.0, -1.0, 1.0)).run {
+            checkSuccessors(0, setOf(0, 1, 3))
+            checkSuccessors(1, setOf(0, 1, 2, 4))
+            checkSuccessors(2, setOf(1, 5, 2))
+            checkSuccessors(3, setOf(0, 3, 4, 6))
+            checkSuccessors(4, setOf(1, 3, 4, 5, 7))
+            checkSuccessors(5, setOf(2, 4, 5, 8))
+            checkSuccessors(6, setOf(3, 6, 7))
+            checkSuccessors(7, setOf(4, 6, 7, 8))
+            checkSuccessors(8, setOf(5, 7, 8))
+
+            checkPredecessors(0, setOf(0, 1, 3))
+            checkPredecessors(1, setOf(0, 1, 2, 4))
+            checkPredecessors(2, setOf(1, 2, 5))
+            checkPredecessors(3, setOf(0, 3, 4, 6))
+            checkPredecessors(4, setOf(1, 3, 4, 5, 7))
+            checkPredecessors(5, setOf(2, 4, 5, 8))
+            checkPredecessors(6, setOf(3, 6, 7))
+            checkPredecessors(7, setOf(4, 6, 7, 8))
+            checkPredecessors(8, setOf(5, 7, 8))
         }
     }
 }
